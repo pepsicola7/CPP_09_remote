4a939beae2de5cf23b804c0944df335fb4bdb055
Peiqi Li <perrine0624@gmail.com>
2025-07-04 20:55:37 +0200

    presque fini

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: peli <peli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 16:58:05 by peli              #+#    #+#             */
-/*   Updated: 2025/07/04 20:43:01 by peli             ###   ########.fr       */
+/*   Updated: 2025/07/04 20:55:09 by peli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -186,20 +186,141 @@ void PmergeMe::sortVector()
     long microseconds = end.tv_usec - start.tv_usec;
     long total_microseconds = seconds * 1000000 + microseconds;
 
-    std::cout << "Time to process a range of " << how_many_chiffre
-              << " elements with std::vector: " << total_microseconds << " us" << std::endl;
+    std::cout << "Time to process a range of " << how_many_chiffre << " elements with std::vector: " << total_microseconds << " us" << std::endl;
 };
 
-void    PmergeMe::sortDeque()
+std::deque<std::deque<int> > make_pair(std::deque<int> vec)
+{
+    std::deque<std::deque<int> > temps;
+    for (size_t i = 0; i + 1 < vec.size(); i += 2)
+    {
+        std::deque<int> element;
+        if (vec[i] > vec[i + 1])
+        {
+            element.push_back(vec[i]);
+            element.push_back(vec[i + 1]);
+        }
+        else
+        {
+            element.push_back(vec[i + 1]);
+            element.push_back(vec[i]);
+        }
+        temps.push_back(element);
+    }
+
+    if (vec.size() % 2 != 0)
+    {
+        std::deque<int> last_pair;
+        last_pair.push_back(vec.back());
+        temps.push_back(last_pair);
+    }
+    return temps;
+}
+
+std::deque<size_t> jacobsthal_sequence_d(size_t n)
+{
+    std::deque<size_t> seq;
+    if (n == 0) 
+        return seq;
+
+    seq.push_back(1);
+    size_t j0 = 0;
+    size_t j1 = 1;
+    while (seq.size() < n) {
+        size_t jn = j1 + 2 * j0;
+        seq.push_back(jn);
+        j0 = j1;
+        j1 = jn;
+    }
+
+    std::deque<size_t> result;
+    std::vector<bool> added(n, false);  // keep as vector for indexing
+    for (size_t i = 0; i < seq.size(); ++i) {
+        if (seq[i] < n && !added[seq[i]]) {
+            result.push_back(seq[i]);
+            added[seq[i]] = true;
+        }
+    }
+
+    for (size_t i = 0; i < n; ++i)
+        if (!added[i])
+            result.push_back(i);
+
+    return result;
+}
+
+void binary_insert_d(std::deque<int>& sorted, int value)
+{
+    size_t left = 0;
+    size_t right = sorted.size();
+    while (left < right)
+    {
+        size_t middle = (left + right) / 2;
+        if (sorted[middle] > value)
+            right = middle;
+        else
+            left = middle + 1;
+    }
+    sorted.insert(sorted.begin() + left, value);
+}
+
+std::deque<int> merge_sort_d(const std::deque<int>& b1)
+{
+    if (b1.size() <= 1)
+        return b1;
+
+    std::deque<std::deque<int> > temps = make_pair(b1);
+    std::deque<int> b2;
+    std::deque<int> smalls;
+
+    for (size_t i = 0; i < temps.size(); i++)
+    {
+        if (temps[i].size() == 2)
+        {
+            b2.push_back(std::max(temps[i][0], temps[i][1]));
+            smalls.push_back(std::min(temps[i][0], temps[i][1]));
+        }
+        else
+        {
+            b2.push_back(temps[i][0]);  // Unpaired last element
+        }
+    }
+
+    std::deque<int> big_sort = merge_sort_d(b2);
+    std::deque<size_t> insertion_order = jacobsthal_sequence_d(smalls.size());
+
+    for (size_t j = 0; j < insertion_order.size(); j++)
+    {
+        size_t index = insertion_order[j];
+        if (index < smalls.size())
+            binary_insert_d(big_sort, smalls[index]);
+    }
+
+    return big_sort;
+}
+
+void PmergeMe::sortDeque()
 {
     struct timeval start, end;
     gettimeofday(&start, NULL);
     
-    
+    std::cout << "Before of deque: ";
+    for (size_t i = 0; i < deqgroups.size(); ++i)
+        std::cout << deqgroups[i] << " ";
+    std::cout << std::endl;
+
+    std::deque<int> sorted = merge_sort_d(deqgroups);
+
+    std::cout << "After of deque: ";
+    for (size_t i = 0; i < sorted.size(); ++i)
+        std::cout << sorted[i] << " ";
+    std::cout << std::endl;
+
     gettimeofday(&end, NULL);
     long seconds = end.tv_sec - start.tv_sec;
     long microseconds = end.tv_usec - start.tv_usec;
     long total_microseconds = seconds * 1000000 + microseconds;
-    std::cout << "Time to process a range of " << how_many_chiffre << " elements with std::deque : " << total_microseconds << " us" << std::endl;
-    return ;
+
+    std::cout << "Time to process a range of " << how_many_chiffre
+              << " elements with std::deque : " << total_microseconds << " us" << std::endl;
 };
