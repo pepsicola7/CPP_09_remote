16d7078ad68ff8ae102de1aaa8ea19c910809ee9
Peiqi Li <perrine0624@gmail.com>
2025-07-04 20:43:24 +0200

    make display better

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: peli <peli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 16:58:05 by peli              #+#    #+#             */
-/*   Updated: 2025/07/04 20:39:26 by peli             ###   ########.fr       */
+/*   Updated: 2025/07/04 20:43:01 by peli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,13 +51,6 @@ void    PmergeMe::parser(char** argv, int argc)
         vecgroups.push_back(value);
         deqgroups.push_back(value);
     }
-    std::cout << "Before: ";
-    for (size_t i = 0; i < vecgroups.size(); i++)
-    {
-
-            std::cout << vecgroups[i] << " ";
-    }
-    std::cout <<std::endl;
 };
 
 std::vector<std::vector<int> > make_pair(std::vector<int> vec)
@@ -176,14 +169,18 @@ void PmergeMe::sortVector()
     struct timeval start, end;
     gettimeofday(&start, NULL);
 
+    std::cout << "Before of vector: ";
+    for (size_t i = 0; i < vecgroups.size(); i++)
+        std::cout << vecgroups[i] << " ";
+    std::cout <<std::endl;
+
     vecgroups = merge_sort(vecgroups);
 
-    std::cout << "After: ";
+    std::cout << "After of vector: ";
     for (size_t i = 0; i < vecgroups.size(); i++)
-    {
         std::cout << vecgroups[i] << " ";
-    }
     std::cout << std::endl;
+
     gettimeofday(&end, NULL);
     long seconds = end.tv_sec - start.tv_sec;
     long microseconds = end.tv_usec - start.tv_usec;
@@ -206,65 +203,3 @@ void    PmergeMe::sortDeque()
     std::cout << "Time to process a range of " << how_many_chiffre << " elements with std::deque : " << total_microseconds << " us" << std::endl;
     return ;
 };
-
-// void    PmergeMe::display()
-// {
-//     std::cout << "After: ";
-//     for (size_t i = 0; i < vecgroups.size(); i++)
-//     {
-//         std::cout << vecgroups[i] << " ";
-//     }
-//     std::cout << std::endl;
-// }; 
-
-// {
-//     if (b1.size() <= 1)
-//         return (b1);
-//     std::vector<std::vector<int> > temps = make_pair(b1);
-
-//     std::vector<int> b2;
-//     std::vector<int> smalls;
-//     for (size_t i = 0; i < temps.size(); i++)
-//     {
-//         if (temps[i].size() == 2)
-//         {
-//             b2.push_back(std::max(temps[i][0], temps[i][1]));
-//             smalls.push_back(std::min(temps[i][0], temps[i][1]));
-//         }
-//         else
-//         b2.push_back(temps[i][0]);
-//     }
-//     for (size_t i = 0; i < b2.size(); i++)
-//         std::cout << "b2"<< b2[i];
-//     std::cout << "-----------" << std::endl;
-//     for (size_t i = 0;i < smalls.size(); i++)
-//         std::cout << "small" << smalls[i];
-//     std::cout << "-----------" << std::endl;
-    
-//     std::vector<int> big_sort = merge_sort(b2);
-    
-    
-//     std::vector<size_t>  insertion_ordre = jacobsthal_sequence(b2.size());
-//     for (size_t j = 0; j < insertion_ordre.size(); j++)
-//     {
-//         size_t index = insertion_ordre[j];
-//         std::cout << "index is : " << index << "temsp.size if : " << temps.size() << std::endl;
-//         if (index < big_sort.size() && temps[index].size() != 1)
-//         {
-//             std::cout << "The value to insert" << temps[index].back() << std::endl;
-//             binary_insert(big_sort, smalls[index]);
-//         }
-//     }
-//     if (temps.back().size() == 1)
-//     binary_insert(big_sort, b2.back());
-//     for (size_t i = 0; i < big_sort.size(); i++)
-//         std::cout << "big_sorted"<< big_sort[i] << std::endl;
-//     std::cout << "-----------" << std::endl;
-//     for (size_t i = 0; i < b2.size(); i++)
-//         std::cout << b2[i] << std::endl;
-//     std::cout << std::endl << "-----------" << std::endl;
-//     for (size_t i = 0;i < smalls.size(); i++)
-//         std::cout<< "small" << smalls[i] << std::endl;
-//     std::cout << "-----------" << std::endl;
-//     return (big_sort);
-// };
